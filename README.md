68d81d3f8a64697c7e8179729e51080bc4957258
Aaron Leao <aaronleao@gmail.com>
2025-02-07 01:35:00 +0100

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # steam_inventory_query
 Steam Inventory Query
+
+## Deps
+```shell
+pip 
+pip install platformdirs
+```
+
+## Fetch Steam profile ID
+[Profile ID finder](https://www.steamidfinder.com/)
+
+
+## How to run
+```shell
+set env variable $steam_api_key
+python steam_inventory_query/cli.py --profile-id $steam_profile_id --api-key $steam_api_key
+```
+
+
+## TODO
+
+- [ ] Add option to list the whole inventory
+- [ ] Add option to filter all Heroes inside inventory
+- [ ] Add option to filter Ward
+- [ ] Add option to filter Courier
+- [ ] Refactor display_inventory function
--- cli.py
+++ cli.py
@@ -1,19 +1,11 @@
-import argparse
-from fetch_inventory import fetch_inventory
-from display_inventory import display_inventory
-
-appid_map = {570, "Dota 2"}
+from steam_inventory_query import parser
+from steam_inventory_query import fs_handler
+from steam_inventory_query import fetch_inventory
+from steam_inventory_query import display_inventory
 
 def main():
-    # Set up argument parser
-    parser = argparse.ArgumentParser(description="Fetch Steam inventory.")
-    parser.add_argument("--profile-id", type=str, required=True, help="Your 17-digit SteamID.")
-    parser.add_argument("--app-id", type=str, default="570", help="The app ID of the game (e.g., 570 for Dota 2).")
-    parser.add_argument("--api-key", type=str, help="Your Steam API key (optional for some games).")
-    parser.add_argument("--overwrite", action="store_true", help="Overwrite the inventory file if it already exists.")
-    args = parser.parse_args()
-    # parser.print_help()
-
+    # Get args
+    args = parser.get_args()
     # Use the provided arguments
     STEAM_ID = args.profile_id
     APP_ID = args.app_id
@@ -21,25 +13,13 @@ def main():
     CONTEXT_ID = "2"  # Default context ID for most games
     OVERWRITE = args.overwrite
 
+    # Create cache dir
+    fs_handler.create_cache_dir()
+
     # Fetch the inventory
-    try:
-        inventory = fetch_inventory(STEAM_ID, APP_ID, CONTEXT_ID, API_KEY, OVERWRITE)
-    except AssertionError as err:
-        return err
+    inventory = fetch_inventory.fetch(STEAM_ID, APP_ID, CONTEXT_ID, API_KEY, OVERWRITE)
 
-    
+    display_inventory.display(inventory)
 
-    if inventory:
-        print("Inventory fetched successfully.")
-
-    display_inventory(inventory)
-
-    ### TODO ###
-    #- Add option to list the whole inventory
-    #- Add option to filter all Heroes inside inventory
-    #- Add option to filter Ward
-    #- Add option to filter Courier
-    #- Refactor display_inventory function
-    
 if __name__ == "__main__":
     main()
